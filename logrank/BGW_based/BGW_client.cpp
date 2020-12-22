5eea30a11117bd04e2e102aa8a826cbb66f4be00
Anat Samohi <anatsamohi@gmail.com>
2020-12-22 10:37:07 +0200

    add BGW client  - start

---
+++ logrank/BGW_based/BGW_client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anat Samohi on 22/12/2020.
+//
+
+#include "BGW_client.h"
---
+++ logrank/BGW_based/BGW_client.h
@@ -0,0 +1,11 @@
+//
+// Created by Anat Samohi on 22/12/2020.
+//
+
+#ifndef SEAL_BGW_CLIENT_H
+#define SEAL_BGW_CLIENT_H
+
+class BGW_client
+{};
+
+#endif // SEAL_BGW_CLIENT_H
